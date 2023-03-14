89985477fef08145a7d3c04c8d31eb9dea34f2ad
Long Hoàng <longhoang.2984@gmail.com>
2023-03-14 22:55:55 +0700

    TeamLogoDataStoreSpy

---
+++ Football/Tests/FootballTests/TeamTests/Team Cache/Helpers/TeamLogoDataStoreSpy.swift
@@ -0,0 +1,46 @@
+//
+//  TeamLogoDataStoreSpy.swift
+//  
+//
+//  Created by Cửu Long Hoàng on 14/03/2023.
+//
+
+import Foundation
+import Football
+
+class TeamLogoDataStoreSpy: TeamLogoDataStore {
+    enum Message: Equatable {
+        case insert(data: Data, for: URL)
+        case retrieve(dataFor: URL)
+    }
+    
+    private(set) var receivedMessages = [Message]()
+    private var retrievalResult: Result<Data?, Error>?
+    private var insertionResult: Result<Void, Error>?
+    
+    func insert(_ data: Data, for url: URL) throws {
+        receivedMessages.append(.insert(data: data, for: url))
+        try insertionResult?.get()
+    }
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        receivedMessages.append(.retrieve(dataFor: url))
+        return try retrievalResult?.get()
+    }
+    
+    func completeRetrieval(with error: Error) {
+        retrievalResult = .failure(error)
+    }
+    
+    func completeRetrieval(with data: Data?) {
+        retrievalResult = .success(data)
+    }
+    
+    func completeInsertion(with error: Error) {
+        insertionResult = .failure(error)
+    }
+    
+    func completeInsertionSuccessfully() {
+        insertionResult = .success(())
+    }
+}
