2c7d1c1fe3761612d5707440909fcddfbde9a8f9
Long Hoàng <longhoang.2984@gmail.com>
2023-03-14 21:52:22 +0700

    TeamStore

---
+++ Football/Sources/Football/Team/Team Cache/TeamStore.swift
@@ -0,0 +1,14 @@
+//
+//  TeamStore.swift
+//  
+//
+//  Created by Cửu Long Hoàng on 14/03/2023.
+//
+
+import Foundation
+
+public protocol TeamStore {
+    func deleteCachedTeam() throws
+    func insert(_ feed: [LocalTeam], timestamp: Date) throws
+    func retrieve() throws -> [LocalTeam]?
+}
