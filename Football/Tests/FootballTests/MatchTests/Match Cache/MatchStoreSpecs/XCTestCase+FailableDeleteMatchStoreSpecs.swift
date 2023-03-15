214a1b2e644a7c087d4e22d088c13a524dc33efe
Long Hoàng <longhoang.2984@gmail.com>
2023-03-15 23:09:08 +0700

    FailableDeleteMatchStoreSpecs

--- Football/Tests/FootballTests/MatchTests/Match Cache/MatchStoreSpecs/XCTestCase+FailableDeleteMatchStoreSpecs.swift
+++ Football/Tests/FootballTests/MatchTests/Match Cache/MatchStoreSpecs/XCTestCase+FailableDeleteMatchStoreSpecs.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import XCTest
+import Football
+
+extension FailableDeleteStoreSpecs where Self: XCTestCase {
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: MatchStore, file: StaticString = #filePath, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+    
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: MatchStore, file: StaticString = #filePath, line: UInt = #line) {
+        deleteCache(from: sut)
+        
+        expect(sut, toRetrieve: .success(.none), file: file, line: line)
+    }
+}
