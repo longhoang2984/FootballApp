26b9cbb66664f953ed46961dcb73b6055237f136
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2023-03-24 18:42:26 +0700

    fix build

--- FootballiOS/Package.swift
+++ FootballiOS/Package.swift
@@ -15,16 +15,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../Football"),
+        .package(name: "Football", path: "../Football")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "FootballiOS",
-            dependencies: []),
+                name: "FootballiOS",
+                dependencies: [.product(name: "Football", package: "Football")],
+                resources: [.copy("Sources")]),
         .testTarget(
             name: "FootballiOSTests",
-            dependencies: ["FootballiOS", "Football"]),
+            dependencies: ["FootballiOS"]),
     ]
 )
